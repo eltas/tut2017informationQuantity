4bd8d89603b6a1dea673a08aa405e3d253a05773
eltas <ge_enemy@icloud.com>
2017-12-23 09:06:11 +0900

    Frequencer.javaとInformationEstimator.javaの補完．テストケース追加．

--- s4/b173365/Frequencer.java
+++ s4/b173365/Frequencer.java
@@ -19,18 +19,27 @@ public class Frequencer implements FrequencerInterface{
     // Code to Test, *warning: This code  contains intentional problem*
     byte [] myTarget;
     byte [] mySpace;
-    public void setTarget(byte [] target) { myTarget = target;}
-    public void setSpace(byte []space) { mySpace = space; }
-    public int frequency() {
-	int targetLength = myTarget.length;
-	int spaceLength = mySpace.length;
+public void setTarget(byte [] target) { myTarget = target;}
+public void setSpace(byte []space) { mySpace = space; }
+public int frequency() {
+	//int targetLength = myTarget.length;
+	//int spaceLength = mySpace.length;
 	int count = 0;
-	for(int start = 0; start<spaceLength; start++) { // Is it OK?
-	    boolean abort = false;
-	    for(int i = 0; i<targetLength; i++) {
-		if(myTarget[i] != mySpace[start+i]) { abort = true; break; }
-	    }
-	    if(abort == false) { count++; }
+	
+	if(myTarget == null || myTarget.length == 0) {
+		count = -1;
+	}
+	else if(mySpace == null || mySpace.length == 0) {
+		count = 0;
+	}
+	else {
+		for(int start = 0; start<mySpace.length; start++) { // Is it OK?
+			boolean abort = false;
+			for(int i = 0; i<myTarget.length; i++) {
+				if(myTarget[i] != mySpace[start+i]) { abort = true; break; }
+				if(abort == false) { count++; }
+			}
+		}
 	}
 	return count;
     }
