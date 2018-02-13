50e2bbb863343d149f503cbde33a1a61bd7c1eef
eltas <ge_enemy@icloud.com>
2018-02-14 02:23:49 +0900

    課題

--- s4/b173365/Frequencer.java
+++ s4/b173365/Frequencer.java
@@ -19,10 +19,12 @@ private void printSuffixArray() {
     if(spaceReady) {
         for(int i=0; i< mySpace.length; i++) {
             int s = suffixArray[i];
-            for(int j=s;j<mySpace.length;j++) {
+            System.out.print("[printSuffixArray]");
+            for(int j=s; j<mySpace.length; j++) {
                 System.out.write(mySpace[j]);
             }
             System.out.write('\n');
+
         }
     }
 }
@@ -58,51 +60,46 @@ private void printSuffixArray() {
         if(mySpace.length>0) spaceReady = true;
         suffixArray = new int[space.length];
         // put all suffixes in suffixArray. Each suffix is expressed by one interger.
-         for(int i = 0; i< space.length; i++) {
-         suffixArray[i] = i;
-         }
-         /*
-         ArrayList<String> list = new ArrayList<String>();
-         for(int i=0; i<mySpace.length; i++) {
-         for(int j=s;j<mySpace.length;j++) {
-         char token;
-         String str;
-         token = mySpace[j];
-         str += token;
-         }
-         list.add(str);
-         }
-         Collection.sort(list);
-         
-         for(int i=0; i<mySpace.length; i++){
-         suffixArray[i] = mySpace.length - list.get(i).length;
-         }
-         */
+        for(int i = 0; i< space.length; i++) {
+            suffixArray[i] = i;
+        }
+//        for(int i = 0; i < mySpace.length; i++){
+//            for(int j = i + 1; j < mySpace.length; j++){
+//                if(suffixCompare(i, j) == 1){
+//                    int tmp = suffixArray[i];
+//                    suffixArray[i] = suffixArray[j];
+//                    suffixArray[j] = tmp;
+//                }
+//            }
+//        }
+//
+//        printSuffixArray();
         
-        // Sorting is not implmented yet. /* Example from "Hi Ho Hi Ho"
-        /*
-         0: Hi Ho
-         1: Ho
-         2: Ho Hi Ho
-         3:Hi Ho
-         4:Hi Ho Hi Ho
-         5:Ho
-         6:Ho Hi Ho
-         7:i Ho
-         8:i Ho Hi Ho
-         9:o
-         A:o Hi Ho
-         */
-        for(int i = 0; i < mySpace.length; i++){
-            for(int j = i + 1; j < mySpace.length; j++){
-                if(suffixCompare(i, j) == 1){
-                    int tmp = suffixArray[i];
-                    suffixArray[i] = suffixArray[j];
-                    suffixArray[j] = tmp;
+        quickSort(suffixArray, 0, suffixArray.length-1);
+        printSuffixArray();
+    }
+    
+    public int[] quickSort(int[] arr, int left, int right) {
+        if(left<=right) {
+            int l = left;
+            int r = right;
+            while(l <= r) {
+                while(suffixCompare(l, (left+right)/2) == -1) l++;
+                while(suffixCompare(r, (left+right)/2) == 1) r--;
+                
+                if(l <= r) {
+                    int tmp = arr[l];
+                    arr[l] = arr[r];
+                    arr[r] = tmp;
+                    l++;
+                    r--;
                 }
             }
+            
+            if (left < r) quickSort(arr, left, r);
+            if (l < right) quickSort(arr, l, right);
         }
-        printSuffixArray();
+        return arr;
     }
 
 private int targetCompare(int i, int j, int end) {
@@ -122,19 +119,62 @@ private int targetCompare(int i, int j, int end) {
 // "Ho" = "Ho"
 //"Ho" < "Ho":"Ho"isnotintheheadofsuffix"Ho"
 // "Ho" = "H" : "H" is in the head of suffix "Ho"
-return 0;
+    
+    int si = suffixArray[i];
+    int s = 0;
+    if(si > s) s = si;
+    int n = mySpace.length - s;
+    if(n > end) n = end; // endはTargetの長さ
+    for(int k = 0; k < n; k++){
+        if(mySpace[si+k] > myTarget[k]) return 1;
+        if(mySpace[si+k] < myTarget[k]) return -1;
+    }
+    if(n < end) return -1;
+    return 0;
 }
 private int subByteStartIndex(int start, int end) { 
 // It returns the index of the first suffix which is equal or greater than subBytes;
 // not implemented yet;
 // For "Ho", it will return 5 for "Hi Ho Hi Ho".
-// For "Ho ", it will return 6 for "Hi Ho Hi Ho". 
-return suffixArray.length;
+// For "Ho ", it will return 6 for "Hi Ho Hi Ho".
+    
+//    for(int i = 0; i < suffixArray.length; i++){
+//        int temp = targetCompare(i,start,end);
+//        if(temp == 0) return i;
+//    }
+    int pLeft = 0;
+    int pRight = suffixArray.length-1;
+    do{
+        int center = (pLeft + pRight)/2;
+        if (targetCompare(center,start,end) == 0 ) {
+            while(targetCompare(center-1,start,end) == 0) center--;
+            return center;
+        } else if (targetCompare(center,start,end) == -1){
+            pLeft = center+1;
+        }
+    }while (pLeft <= pRight);
+    return suffixArray.length;
 }
 private int subByteEndIndex(int start, int end) {
 // It returns the next index of the first suffix which is greater than subBytes; // not implemented yet
 // For "Ho", it will return 7 for "Hi Ho Hi Ho".
 // For "Ho ", it will return 7 for "Hi Ho Hi Ho".
+    int pLeft = 0;
+    int pRight = suffixArray.length-1;
+    do{
+        int center = (pLeft + pRight) / 2;
+        if (targetCompare(center,start,end) == 0) {
+            while(targetCompare(center+1,start,end) == 0) center++;
+            return center;
+        } else if (targetCompare(center,start,end) == 1){
+            pRight = center-1;
+        }
+    }while (pLeft <= pRight);
+//    for(int i = 0; i < suffixArray.length; i++){
+//        int temp = targetCompare(i,start,end);
+//        if(temp == 1) return i;
+//    }
+    
     return suffixArray.length;
 } 
 public int subByteFrequency(int start, int end) {
@@ -147,31 +187,11 @@ for(int i = 0; i< (end - start); i++) {
 if(myTarget[start+i] != mySpace[offset+i]) { abort = true; break; } } 
 if(abort == false) { count++; } } 
 */
-int first = subByteStartIndex(start,end);
-int last1 = subByteEndIndex(start, end);
-/* inspection code
-for(int k=start;k<end;k++) { System.out.write(myTarget[k]); } System.out.printf(": first=%d last1=%d\n", first, last1);
-*/
-
-    String temp = new String(mySpace);
-    String temp2 = new String(myTarget);
     
-    for (int i=0; i<suffixArray.length; i++)
-    {
-        if(temp.substring(suffixArray[i],suffixArray.length).startsWith(temp2)) {
-            first = i;
-            break;
-        }
-    }
-    for (int i=0; i<suffixArray.length; i++)
-    {
-        if(temp.substring(suffixArray[suffixArray.length-i-1],suffixArray.length).startsWith(temp2)) {
-            last1 = suffixArray.length-i;
-            break;
-        }
-    }
+    int first = subByteStartIndex(start,end);
+    int last1 = subByteEndIndex(start, end);
     
-return last1 - first;
+    return last1 - first + 1;
 } 
 public void setTarget(byte [] target) { 
    myTarget = target;
@@ -184,24 +204,24 @@ public int frequency() {
     //return 0;
 } 
 
-    public static void main(String[] args) {
-        Frequencer frequencerObject;
-        try {
-            frequencerObject = new Frequencer();
-            frequencerObject.setSpace("Hi Ho Hi Ho".getBytes());
-            
-            frequencerObject.setTarget("H".getBytes());
-            int result = frequencerObject.frequency();
-            System.out.print("Freq = "+ result+" ");
-            if(4 == result) {
-                System.out.println("OK");
-            }
-            else {System.out.println("WRONG");
-            }
+public static void main(String[] args) {
+    Frequencer frequencerObject;
+    try {
+        frequencerObject = new Frequencer();
+        frequencerObject.setSpace("Hi Ho Hi Ho".getBytes());
+        
+        frequencerObject.setTarget("H".getBytes());
+        int result = frequencerObject.frequency();
+        System.out.print("Freq = "+ result+" ");
+        if(4 == result) {
+            System.out.println("OK");
         }
-        catch(Exception e) {
-            System.out.println("STOP");
+        else {System.out.println("WRONG");
         }
     }
+    catch(Exception e) {
+        System.out.println("STOP");
+    }
+}
 }
 
